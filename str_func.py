869d77aa707ccfe0a5ff74243f6662eb7bbfc8d0
PC <sabyrsupcik@gmail.com>
2024-01-26 12:41:15 +0300

    add first_upper_str

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def upper_str(text):
     return text.upper()
 print(upper_str("Привет Мир"))
+
+def first_upper_str(text2):
+    """docstring2"""
+    return text2.title()
+print(first_upper_str("привет мир"))
